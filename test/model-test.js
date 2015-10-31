bf832d96b41768fd6e91d6b54f7300d11788d7fb
anserran <angelserranolaguna@gmail.com>
2015-10-31 09:59:54 +0100

    Add model checking

---
+++ test/model-test.js
@@ -0,0 +1,88 @@
+var Collection = require('../lib/collection');
+
+var MongoClient = require('mongodb').MongoClient;
+var db;
+
+module.exports = {
+    setUp: function (callback) {
+        MongoClient.connect('mongodb://127.0.0.1:27017/easy-collection-test', function (err, database) {
+            if (err) {
+                console.log(err);
+            }
+            db = database;
+            db.dropDatabase(function () {
+                callback();
+            });
+        });
+    },
+    tearDown: function (callback) {
+        db.dropDatabase(function () {
+            db.close();
+            callback();
+        });
+    },
+    testModel: function (test) {
+
+        test.expect(7);
+
+        var model = {
+            'user': {
+                type: 'string',
+                required: true
+            },
+            'password': {
+                type: 'string',
+                required: true
+            },
+            'age': {
+                type: 'number'
+            },
+            'married': {
+                type: 'boolean'
+            },
+            'resources': {
+                type: 'object',
+                model: {
+                    'create': {
+                        type: 'number',
+                        required: true
+                    },
+                    'read': {
+                        type: 'boolean'
+                    }
+                }
+            }
+        };
+
+        var collection = new Collection(db, 'model-collection', model);
+
+        collection.insert({})
+            .fail(function (err) {
+                test.strictEqual(err.code, 'E_INVALID_DOCUMENT')
+                return collection.insert({
+                    user: 'admin',
+                    password: 'admin',
+                    age: 10,
+                    married: true,
+                    resources: {create: 5, read: false}
+                });
+            }).then(function (user) {
+                test.ok(user);
+                return collection.findAndModify(user._id, {age: 14, married: false})
+            }).then(function (user) {
+                test.strictEqual(user.age, 14);
+                test.strictEqual(user.married, false);
+                return collection.findAndModify(user._id, {age: 'ñor'})
+            }).fail(function (err) {
+                test.strictEqual(err.code, 'E_INVALID_DOCUMENT');
+                return collection.insert({user: 'admin', password: 'admin', resources: {}})
+            }).fail(function (err) {
+                test.strictEqual(err.code, 'E_INVALID_DOCUMENT');
+                return collection.insert({user: 'admin', password: 'admin', whatever: 'ñor'});
+            }).fail(function (err) {
+                test.strictEqual(err.code, 'E_INVALID_DOCUMENT');
+            }).then(function () {
+                test.done()
+            });
+    }
+};
